d57b36c82fe787ce40db7d35133e52573c54a145
Secretmm <1584949586@qq.com>
2016-06-13 22:44:48 +0800

    fourtime

--- js/my.js
+++ js/my.js
@@ -10,14 +10,3 @@ var str="周密密--期待成为一个web前端工程师";
             }
             var done=setInterval("showword()",300);
 
-var str="周密密--期待成为一个web前端工程师";
-            var len=str.length;
-            var i=1;
-            function showword(){
-                document.getElementById("show").innerHTML=str.substr(0,i);
-                i++;
-                if(i>len){
-                clearInterval("done");
-                }
-            }
-            var done=setInterval("showword()",300);
