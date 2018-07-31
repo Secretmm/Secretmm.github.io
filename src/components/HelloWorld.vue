a2040c44f44503c78bc4825b83b988040939bcc3
secretmm <1584949586@qq.com>
2018-07-31 16:11:36 +0800

    初始化项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>6666</div>
+</template>
+<style scoped>
+</style>
+<script>
+    export default {
+        name: 'HelloWorld',
+        components: {
+        },
+        props: {},
+        data() {
+            return {
+            }
+        },
+        computed: {
+        },
+        watch: {
+        },
+        methods: {
+        },
+        created() {
+        },
+        destroyed() {
+        },
+        mounted() {
+        }
+    }
+</script>
