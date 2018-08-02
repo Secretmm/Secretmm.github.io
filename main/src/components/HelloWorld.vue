9e3312cf300f37856285664467efe4ba7082540e
secretmm <1584949586@qq.com>
2018-08-02 15:06:41 +0800

    团子掉落

--- main/src/components/HelloWorld.vue
+++ main/src/components/HelloWorld.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="main">
-        <img :src="GifImg[`index${i}`]" 
-            v-for="i in 24" :key="i" 
-            class="img-one" 
-            :style="{'transform':`translateY(${Math.floor(Math.random()*5 + i * 10)}px)`}">
+        <img :src="GifImg[`index${item.img}`]" 
+            v-if="item.img"
+            v-for="(item, index) in showImgList"
+            :key="item.id"
+            :style="item.style"
+            class="img-one">
     </div>
 </template>
 <style scoped>
@@ -11,14 +13,29 @@
         width: 100%;
         height: 100%;
         /* background-color: rgb(142, 178, 149) */
-        background-color: rgb(165, 200, 142);
+        background-color: rgba(165, 200, 142, .4);
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        position: relative;
+        overflow: hidden;
     }
     .img-one {
         width: 80px;
         height: 80px;
-        transform: translateY(30)
+        transform: translateY(30);
+        position: absolute;
+        animation-name: dropDown;
+        animation-duration: 7.5s;
+        animation-timing-function: ease-in;
+        animation-fill-mode: backwards;
+        /* animation-iteration-count:infinite */
     }
-
+    @keyframes dropDown
+    {
+        0%{transform: translateY(0);}
+        100%{transform: translateY(1000px);}
+    }
 </style>
 <script>
     import * as GifImg from  '@/importImage/gifImg'
@@ -29,6 +46,8 @@
         props: {},
         data() {
             return {
+                showImgList: [],
+                addImg: null,
                 GifImg,
             }
         },
@@ -37,12 +56,41 @@
         watch: {
         },
         methods: {
+            // 随机出现的图片
+            showImg(id) {
+                if(this.showImgList.length <= 15){
+                    this.showImgList.push({
+                        id: id,
+                        img: Math.ceil(Math.random() * 24),
+                        style: {
+                            top: '-80px',
+                            left: `calc(${Math.floor(Math.random() * 100)}% - 40px)`,
+                            // animationDelay: `${Math.floor(Math.random() * 9)}`,
+                        },
+                    });
+                } else {
+                    this.showImgList.splice(0, this.showImgList.length -15);
+                    // this.showImgList.shift();
+                }
+            },
+            showAloneImg() {
+
+            },
         },
         created() {
         },
         destroyed() {
         },
         mounted() {
+            let id = 1;
+            this.showImg(id);
+    
+            this.addImg = setInterval(() => {
+                if(this.showImgList.length <= 15) {
+                    id ++;
+                }
+                this.showImg(id);
+            }, 250);
         }
     }
 </script>
